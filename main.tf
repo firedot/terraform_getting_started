af779eebdd878c363ba583b3c731cdd40934adf3
Aleksandar Todorov <linked2u@gmail.com>
2019-04-16 14:01:13 +0300

    added basic Terraform configuration

---
+++ main.tf
@@ -0,0 +1,24 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "ami_id" {}
+variable "key_name" {}
+variable "region_id" {}
+
+variable "vpc_security_group_ids" {
+  type = "list"
+}
+
+variable "ssh_username" {}
+variable "aws_arn" {}
+
+provider "aws" {
+access_key = "${var.aws_access_key}"
+secret_key = "${var.aws_secret_key}"
+region = "${var.region_id}"
+}
+
+resource "aws_instance" "example" {
+ami = "${var.ami_id}"
+instance_type = "t2.micro"
+}
+
